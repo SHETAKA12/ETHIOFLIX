5d32342d092bab47c62202d4f0a2e6914bab351c
SHETAKA12 <kaleagle12@gmail.com>
2024-06-29 14:20:08 +0300

    imported AOS to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import ForYou from './components/forYou/forYou'
+import AOS from 'aos'
+import {useEffect } from 'react'
 const App = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: 'ease-in-out-sine',
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+  
   return (
     <div >
       <Navbar/>
